98d39f52905ae59efbcff906945bd1e583a55768
lKyosukel <144384172+lKyosukel@users.noreply.github.com>
2023-09-23 21:19:13 -0400

    .

--- Week 3/script.js
+++ Week 3/script.js
@@ -3,7 +3,7 @@ const txtInput = document.querySelector(".inputs input"),/*Takes inputs from the
 checkBtn = document.querySelector(".inputs button"),/*Enables button to be clicked when text is entered into the input box*/
 infoTxt = document.querySelector(".info-txt");
 let filterInput;
-checkBtn.addEventListener("click", () => {/*Adds a listener for the specified event letting the program know to run once it occurs*/
+checkBtn.addEventListener("click", () => {/*Adds a listener for the specified event letting the program know to run once it occurs, in this case its a click*/
     let reverseInput = filterInput.split("").reverse().join("");/*Split creates an array, reverse reverses the array, and join puts it back together basically takes the input and flips it*/
     infoTxt.style.display = "block";
     if(filterInput != reverseInput) {/*If the return is not the same then the output below will display same for the follwing line of code for palindromes*/
